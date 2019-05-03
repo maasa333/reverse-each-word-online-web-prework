93d9616dd294d5aeb85b4be5a64766c687fef99f
Maasa Kono <konomaasa@gmail.com>
2019-05-03 23:48:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,12 @@ def reverse_each_word(string)
   end
   new_array.join(" ")
 end
+
+def collect_each_word(string)
+  array = string.split(" ")
+  new_array = []
+  array.collect do |w|
+    new_array << w.reverse
+  end
+  new_array.join(" ")
+end
